32dd872de6525d1c010ffea191f6a0cd62bec2c1
Abdul Ahad <121622598+AbdulAhad7692@users.noreply.github.com>
2023-01-30 15:13:19 +0500

    till cahpter 2

---
+++ assinment 2/app.js
@@ -0,0 +1,43 @@
+// CHAPTER 2 //
+
+var username;
+
+var Myname = "Abdul Ahad";
+
+var message = "Hello World";
+
+alert(message);
+
+var student;
+
+student = "Johne Doe";
+
+alert (student)
+
+student = "15 year old";
+
+alert(student);
+
+student = "Certified Mobile Application Development";
+
+alert(student);
+
+var pizza = "p i z z a \np i z z \np i z \np i \np ";
+
+alert(pizza);
+
+var email = "My Email Address is ";
+
+var email2 = " helloahadadhi@gmail.com";
+
+alert(email+email2);
+
+var book= "A smarter way to learn java script";
+
+alert(book);
+
+document.write("<h1>I Can Write HTML Content using JAVA SCRIPT</h1>")
+
+var ajjeb = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+alert(ajjeb)
