4025d9d5f3042c9d91cdcb34b7107d8aa13981d1
Abdul Ahad <121622598+AbdulAhad7692@users.noreply.github.com>
2023-02-01 21:39:58 +0500

    complete

--- assinment 2/app.js
+++ assinment 2/app.js
@@ -1,4 +1,4 @@
-// CHAPTER 2 //
+ // CHAPTER 2 //
 
 var username;
 
@@ -41,3 +41,47 @@ document.write("<h1>I Can Write HTML Content using JAVA SCRIPT</h1>")
 var ajjeb = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
 
 alert(ajjeb)
+
+
+// CHAPTER 3 //
+
+var age = "hello I'm 17";
+
+alert(age)
+
+var site = "you have visited this site 100 times";
+
+alert(site)
+
+var birthYear = "Born in 2005 finally gonna 18\n";
+
+document.write(birthYear)
+
+var customer1 = "\n John Doe ordered 5 T-shirts on XYZ clothing store";
+
+document.write(customer1)
+
+//  CHAPTER 4 //
+
+// var numA,numB,numC;
+
+// // Illegal Variables //
+
+// var 1variable;
+// var 2 variable;
+// var alert;
+// var space in variable;
+// var 5th variable;
+
+// // Legal Variables //
+
+// var Variable;
+// var $leal;
+// var also_a_variable;
+// var Another_Legal_Variable;
+// var imAlsoLegal;
+
+document.write("<h1>Rules For Naming JS Variable\n</h1>")
+document.write("a variable can only contain letter,number Dollarsign and underscore For example $my_1stVariable\n")
+document.write("Variables names are case sensitive \n" )
+document.write("Variables Names shpuld not be JS keywords \n")
